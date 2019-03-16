54ff94809c8c56999d49aeacdd5103c13071d4a9
Simon Horlick <simonhorlick@gmail.com>
2019-03-16 18:28:01 +0800

    Fix import

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { Counter } from "./greeter";
+import { Counter } from "./counter";
 
 ReactDOM.render(<Counter />, document.getElementById("app"));
